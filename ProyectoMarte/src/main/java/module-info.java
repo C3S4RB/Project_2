dc354bc02925831164721e7aa4bfe05cecb7b408
C3S4RB <cebopaul@espol.edu.ec>
2022-01-14 09:36:13 -0500

    g

--- ProyectoMarte/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.proyectomarte {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.proyectomarte to javafx.fxml;
-    exports com.mycompany.proyectomarte;
-}
